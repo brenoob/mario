547e101c363a5cae01452d40ab63fa0178598058
Breno Batista <breno_1993@outlook.com>
2022-07-31 19:15:20 -0300

    adição de controles pular e recarregar atraves do teclado

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # mario
 jogo de mario
 
+controles
+pular seta para cima ou p
+recarregar r
+
 
 https://user-images.githubusercontent.com/57812022/181151501-2b237871-4fee-42a7-bb43-1e6d441dac14.mp4
 
--- js/script.js
+++ js/script.js
@@ -11,15 +11,16 @@ const jump = () => {
 };
 
 const loop = setInterval(() => {
+  
   const pipePosition = pipe.offsetLeft;
   const goonbaPosition = goonba.offsetLeft;
   const marioPosition = +window
     .getComputedStyle(mario)
     .bottom.replace("px", ""); //o + aqui transforma a string em number
-  console.log(marioPosition);
+  /* console.log(marioPosition); */ //verificando altura do pulo do mario
 
-  if (pipePosition <= 90 && pipePosition > 0 && marioPosition < 80 ||goonbaPosition <= 50 && goonbaPosition > 0 && marioPosition < 80) {
-    //depois do pipe -35
+  if (pipePosition <= 90 && pipePosition > 0 && marioPosition < 80 ||goonbaPosition <= 90 && goonbaPosition > 0 && marioPosition < 50) {
+    
     pipe.style.animation = "none";
     pipe.style.left = `${pipePosition}px`;
 
@@ -32,15 +33,28 @@ const loop = setInterval(() => {
     mario.src = "./img/game-over.png";
     mario.style.width = "50px";
     mario.style.marginLeft = "50px";
-
+    
     clearInterval(loop);
   }
 }, 10);
-document.addEventListener("keydown", jump);
+
+// button jump
+window.addEventListener("keydown", function(mario) {
+  if(mario.key === "p" || mario.key ==="ArrowUp" ){
+  return jump()
+}});
 
 // restart
 const restart = document.querySelector("#refresh");
 
+window.addEventListener("keydown", function (restart) {
+  if(restart.key === "r"){
+    location.reload();
+  }
+});
 restart.addEventListener("click", function () {
   location.reload();
-});
+});/* 
+window.addEventListener("keydown", function(mario) {
+  console.log(mario.key)
+}); */ //verificando eventos do teclado
